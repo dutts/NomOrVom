7dbaa27beac2b5e9f5c39142d1ecc3c5ad3af8c5
dutts <richard@dutton.me.uk>
2016-08-22 22:27:30 +0100

    Brought back in some support for HH

--- webextension/addon/api.js
+++ webextension/addon/api.js
@@ -36,134 +36,158 @@ function ApplyFilter(ratingFilterRange, restaurantEntries, excludeNoData) {
 	});
 }
 
-var restaurantEntries = document.querySelectorAll('div.restaurant:not(.offlineRestaurant)');
-
-var config = document.createElement('div');
-config.id = "nomorvom_config"
-
-var sliderLabel = document.createElement('p');
-sliderLabel.id = "nomorvom_config_title";
-sliderLabel.appendChild(document.createTextNode("Move the sliders to filter results by hygiene rating:"));
-config.appendChild(sliderLabel);
-
-var scoreFilterSlider = document.createElement('div');
-scoreFilterSlider.id = "scoreFilterSlider";
-$(scoreFilterSlider).slider({
-	range: true,
-	values: [0, 5],
-	min: 0,
-	max: 5,
-	step: 1,
-	slide: function( event, ui ) {
-		ApplyFilter(ui.values, restaurantEntries, document.getElementById('nomorvom_config_excludeNoData_checkbox').checked);
-	}
-});
-
-var vals = $(scoreFilterSlider).slider("option", "max") - $(scoreFilterSlider).slider("option", "min");
-
-// Space out values
-for (var i = 0; i <= vals; i++) {
-	var el = $('<label>'+(i)+'</label>').css('left',(i/vals*100)+'%');
-
-	$(scoreFilterSlider).append(el);
-}
-
-config.appendChild(scoreFilterSlider);
-
-var excludeNoDataLabel = document.createElement('p');
-excludeNoDataLabel.id = "nomorvom_config_excludeNoData";
-excludeNoDataLabel.appendChild(document.createTextNode("Exclude 'No Result' Entries:"));
-
-var excludeNoDataCheckbox = document.createElement('input');
-excludeNoDataCheckbox.type = "checkbox"
-excludeNoDataCheckbox.id = "nomorvom_config_excludeNoData_checkbox";
-excludeNoDataCheckbox.checked = true;
-excludeNoDataCheckbox.addEventListener('change', function() {
-	ApplyFilter($(scoreFilterSlider).slider("values"), restaurantEntries, excludeNoDataCheckbox.checked);
-});
-excludeNoDataLabel.appendChild(excludeNoDataCheckbox);
-
-config.appendChild(excludeNoDataLabel);
-
-var restaurantsDiv = document.querySelector("div.restaurants");
-restaurantsDiv.insertBefore(config, restaurantsDiv.firstChild);
-
-var port = chrome.runtime.connect({name:"scorelookup"});
-
-// Set up the listener for the result returned from the addon script
-port.onMessage.addListener(function(restaurantScore) {
-	// find the score placeholder for the restaurant we've got a result for
-	var restaurantScorePlaceholder = document.querySelector("div.restaurant[data-nomorvom-id='"+restaurantScore.id+"'] div#nomorvom");
-	restaurantScorePlaceholder.setAttribute('data-rating', restaurantScore.rating);
-	RemoveElement('p#nomorvom_loading', restaurantScorePlaceholder);
-	RemoveElement('div#nomorvom_progressbar', restaurantScorePlaceholder);
-	
-	if (restaurantScore.rating > 0) {
-		for (var i = 0; i < restaurantScore.rating; i++) {
-			AppendImg(restaurantScorePlaceholder, '48-fork-and-knife-icon.png');
-		}
-		for (var i = 0; i < 5 - restaurantScore.rating; i++) {
-			AppendImg(restaurantScorePlaceholder, 'toilet-paper-icon_32.png');
-		}
-	}
-
-	var resultText = document.createElement('div');
-	resultText.id = "nomorvom_hygieneScore"
-
-	if (restaurantScore.rating == "AwaitingInspection") {
-		resultText.textContent = "This takeaway is awaiting inspection";					
-		restaurantScore.rating = 0;
-	}	
-	else {
-		if (restaurantScore.rating == -1) {
-			resultText.textContent = "Sorry, no food hygiene data found";
-		}
-		else {
-			resultText.textContent = "Hygiene Score : " + restaurantScore.rating + "/5";
-		}
-		resultText.appendChild(document.createElement('br'));
-		resultText.appendChild(document.createTextNode("Rated on " + restaurantScore.date.substring(0, 10)));
-	}
-	restaurantScorePlaceholder.appendChild(resultText);
-
-	// Filter accordingly
-	ApplyFilter($(scoreFilterSlider).slider("values"), restaurantEntries, document.getElementById('nomorvom_config_excludeNoData_checkbox').checked);
-});
-
-var restaurantId = 0;
-
-Array.prototype.forEach.call(restaurantEntries, function (el, i) {
-
-    var name = el.querySelector('h2.name').textContent.trim(); 
-    var address = el.querySelector('p.address').childNodes[0].textContent.trim();
-
-	port.postMessage({id:restaurantId, name:name, address:address});
-    
+function CreateScorePlaceholderElement(loadingImageSource) {
     var scorePlaceholder = document.createElement('div');
 	scorePlaceholder.id = "nomorvom";
-	
 	var loadingText = document.createElement('p');
 	loadingText.id = "nomorvom_loading";
 	loadingText.textContent = "Loading food scores...";
-	
     var loaderImg = document.createElement('div');
 	loaderImg.id = "nomorvom_progressbar";
-
 	var img = new Image();
 	img.onload = function() {
   		loaderImg.appendChild(img);
 	};
-
-	img.src = chrome.extension.getURL('loading.gif');
-	
+	img.src = loadingImageSource;
 	scorePlaceholder.appendChild(loadingText);
 	scorePlaceholder.appendChild(loaderImg);
-	
 	scorePlaceholder.setAttribute('data-rating', 0);
-	
-	el.setAttribute('data-nomorvom-id', restaurantId);
+	return scorePlaceholder;
+}
 
-    el.appendChild(scorePlaceholder);
- 
-    restaurantId++;
-});
+function CreateConfigElement() {
+	var config = document.createElement('div');
+	config.id = "nomorvom_config"
+	var sliderLabel = document.createElement('p');
+	sliderLabel.id = "nomorvom_config_title";
+	sliderLabel.appendChild(document.createTextNode("Move the sliders to filter results by hygiene rating:"));
+	config.appendChild(sliderLabel);
+	var scoreFilterSlider = document.createElement('div');
+	scoreFilterSlider.id = "scoreFilterSlider";
+	$(scoreFilterSlider).slider({
+		range: true,
+		values: [0, 5],
+		min: 0,
+		max: 5,
+		step: 1,
+		slide: function( event, ui ) {
+			ApplyFilter(ui.values, restaurantEntries, document.getElementById('nomorvom_config_excludeNoData_checkbox').checked);
+		}
+	});
+	var vals = $(scoreFilterSlider).slider("option", "max") - $(scoreFilterSlider).slider("option", "min");
+	// Space out values
+	for (var i = 0; i <= vals; i++) {
+		var el = $('<label>'+(i)+'</label>').css('left',(i/vals*100)+'%');
+
+		$(scoreFilterSlider).append(el);
+	}
+	config.appendChild(scoreFilterSlider);
+	var excludeNoDataLabel = document.createElement('p');
+	excludeNoDataLabel.id = "nomorvom_config_excludeNoData";
+	excludeNoDataLabel.appendChild(document.createTextNode("Exclude 'No Result' Entries:"));
+	var excludeNoDataCheckbox = document.createElement('input');
+	excludeNoDataCheckbox.type = "checkbox"
+	excludeNoDataCheckbox.id = "nomorvom_config_excludeNoData_checkbox";
+	excludeNoDataCheckbox.checked = true;
+	excludeNoDataCheckbox.addEventListener('change', function() {
+		ApplyFilter($(scoreFilterSlider).slider("values"), restaurantEntries, excludeNoDataCheckbox.checked);
+	});
+	excludeNoDataLabel.appendChild(excludeNoDataCheckbox);
+	config.appendChild(excludeNoDataLabel);
+	return config;
+}
+
+// Just-Eat
+if (window.location.href.indexOf("just-eat.co.uk") > -1) {
+	var restaurantEntries = document.querySelectorAll('div.restaurant:not(.offlineRestaurant)');
+	var config = CreateConfigElement();
+	var restaurantsDiv = document.querySelector("div.restaurants");
+	restaurantsDiv.insertBefore(config, restaurantsDiv.firstChild);
+	var port = chrome.runtime.connect({name:"scorelookup"});
+	port.onMessage.addListener(function(restaurantScore) {
+		var restaurantScorePlaceholder = document.querySelector("div.restaurant[data-nomorvom-id='"+restaurantScore.id+"'] div#nomorvom");
+		restaurantScorePlaceholder.setAttribute('data-rating', restaurantScore.rating);
+		RemoveElement('p#nomorvom_loading', restaurantScorePlaceholder);
+		RemoveElement('div#nomorvom_progressbar', restaurantScorePlaceholder);
+		if (restaurantScore.rating > 0) {
+			for (var i = 0; i < restaurantScore.rating; i++) {
+				AppendImg(restaurantScorePlaceholder, '48-fork-and-knife-icon.png');
+			}
+			for (var i = 0; i < 5 - restaurantScore.rating; i++) {
+				AppendImg(restaurantScorePlaceholder, 'toilet-paper-icon_32.png');
+			}
+		}
+		var resultText = document.createElement('div');
+		resultText.id = "nomorvom_hygieneScore"
+		if (restaurantScore.rating == "AwaitingInspection") {
+			resultText.textContent = "This takeaway is awaiting inspection";					
+			restaurantScore.rating = 0;
+		}	
+		else {
+			if (restaurantScore.rating == -1) {
+				resultText.textContent = "Sorry, no food hygiene data found";
+			}
+			else {
+				resultText.textContent = "Hygiene Score : " + restaurantScore.rating + "/5";
+			}
+		}
+		restaurantScorePlaceholder.appendChild(resultText);
+		ApplyFilter($(scoreFilterSlider).slider("values"), restaurantEntries, document.getElementById('nomorvom_config_excludeNoData_checkbox').checked);
+	});
+	var restaurantId = 0;
+	Array.prototype.forEach.call(restaurantEntries, function (el, i) {
+	    var name = el.querySelector('h2.name').textContent.trim(); 
+	    var address = el.querySelector('p.address').childNodes[0].textContent.trim();
+		port.postMessage({id:restaurantId, name:name, address:address});
+		var scorePlaceholder = CreateScorePlaceholderElement(chrome.extension.getURL('loading.gif'));		
+		el.setAttribute('data-nomorvom-id', restaurantId);
+	    el.appendChild(scorePlaceholder);
+	    restaurantId++;
+	});
+}
+
+// Hungry House
+if (window.location.href.indexOf("hungryhouse.co.uk") > -1) {
+	var restaurantEntries = document.querySelectorAll('div.restsSearchItemRes'); 
+	var port = chrome.runtime.connect({name:"linkedPageScoreLookup"});
+	port.onMessage.addListener(function(restaurantScore) {
+		var restaurantScorePlaceholder = document.querySelector("div.restsSearchItemRes[data-nomorvom-id='"+restaurantScore.id+"'] div#nomorvom");
+		restaurantScorePlaceholder.setAttribute('data-rating', restaurantScore.rating);
+		RemoveElement('p#nomorvom_loading', restaurantScorePlaceholder);
+		RemoveElement('div#nomorvom_progressbar', restaurantScorePlaceholder);
+		if (restaurantScore.rating > 0) {
+			for (var i = 0; i < restaurantScore.rating; i++) {
+				AppendImg(restaurantScorePlaceholder, '48-fork-and-knife-icon.png');
+			}
+			for (var i = 0; i < 5 - restaurantScore.rating; i++) {
+				AppendImg(restaurantScorePlaceholder, 'toilet-paper-icon_32.png');
+			}
+		}
+		var resultText = document.createElement('div');
+		resultText.id = "nomorvom_hygieneScore"
+		if (restaurantScore.rating == "AwaitingInspection") {
+			resultText.textContent = "This takeaway is awaiting inspection";					
+			restaurantScore.rating = 0;
+		}	
+		else {
+			if (restaurantScore.rating == -1) {
+				resultText.textContent = "Sorry, no food hygiene data found";
+			}
+			else {
+				resultText.textContent = "Hygiene Score : " + restaurantScore.rating + "/5";
+			}
+		}
+		restaurantScorePlaceholder.appendChild(resultText);
+	});
+	var restaurantId = 0;
+	Array.prototype.forEach.call(restaurantEntries, function (el, i) {
+	    var name = el.querySelector('a.restPageLink').textContent.trim(); 
+    	var pageUri = el.querySelector('a.restPageLink').getAttribute('href').trim();
+		var fullPageUri = window.location.protocol + "//" + window.location.host + pageUri;
+		port.postMessage({id:restaurantId, name:name, fullPageUri:fullPageUri});	    
+	    var scorePlaceholder = CreateScorePlaceholderElement(chrome.extension.getURL('loading.gif'));
+		el.setAttribute('data-nomorvom-id', restaurantId);
+	    el.appendChild(scorePlaceholder);
+	    restaurantId++;
+	});
+}
--- webextension/addon/background.js
+++ webextension/addon/background.js
@@ -1,43 +1,102 @@
+var parseHTML = function(str) {
+	var tmp = document.implementation.createDocument();
+	tmp.documentElement.appendChild(str)
+	return tmp.body.children;
+}
+
 chrome.runtime.onConnect.addListener(function(port){
-	console.assert(port.name == "scorelookup");
-	port.onMessage.addListener(function(restaurant) {
-
-		var postcodeRegexp = /[A-Z]{1,2}[0-9]{1,2}[A-Z]{0,1} [0-9][A-Z]{2}/;
-		var postcodeIndex = restaurant.address.search(postcodeRegexp);
-		var address;
-		if (postcodeIndex === -1) {
-			// this should never happen
-			address = restaurant.address;
-		}
-		else {
-			address = restaurant.address.substring(postcodeIndex);
-		}
-
-  		var url = "http://api.ratings.food.gov.uk/Establishments?name=" + encodeURIComponent(restaurant.name) + "&address=" + encodeURIComponent(address); 
-
-		var rating = 0;
-		var ratingDate = '';
-
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4)
-			{
-				var resp = JSON.parse(xhr.responseText);
-				if (resp.establishments.length > 0) {
-					rating = resp.establishments[0].RatingValue;
-					ratingDate = resp.establishments[0].RatingDate;
-				}
-				else {
-					rating = -1;
-				}
-				console.log(restaurant.id + " " + rating);
-				port.postMessage({id:restaurant.id, rating:rating, date:ratingDate});
+	if(port.name == "scorelookup") {
+		port.onMessage.addListener(function(restaurant) {
+			var postcodeRegexp = /[A-Z]{1,2}[0-9]{1,2}[A-Z]{0,1} [0-9][A-Z]{2}/;
+			var postcodeIndex = restaurant.address.search(postcodeRegexp);
+			var address;
+			if (postcodeIndex === -1) {
+				// this should never happen
+				address = restaurant.address;
 			}
-		};
-		xhr.open("GET", url, true);
-		xhr.setRequestHeader('x-api-version', 2);
-		xhr.setRequestHeader('Content-Type','application/json');
-		xhr.setRequestHeader('Accept','application/json');
-		xhr.send();
-  	});
+			else {
+				address = restaurant.address.substring(postcodeIndex);
+			}
+
+	  		var url = "http://api.ratings.food.gov.uk/Establishments?name=" + encodeURIComponent(restaurant.name) + "&address=" + encodeURIComponent(address); 
+
+			var rating = 0;
+			var ratingDate = '';
+
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState == 4)
+				{
+					var resp = JSON.parse(xhr.responseText);
+					if (resp.establishments.length > 0) {
+						rating = resp.establishments[0].RatingValue;
+						ratingDate = resp.establishments[0].RatingDate;
+					}
+					else {
+						rating = -1;
+					}
+					console.log(restaurant.id + " " + rating);
+					port.postMessage({id:restaurant.id, rating:rating, date:ratingDate});
+				}
+			};
+			xhr.open("GET", url, true);
+			xhr.setRequestHeader('x-api-version', 2);
+			xhr.setRequestHeader('Content-Type','application/json');
+			xhr.setRequestHeader('Accept','application/json');
+			xhr.send();
+	  	});
+	}
+	if(port.name == "linkedPageScoreLookup") {
+		port.onMessage.addListener(function(restaurant) {
+			//console.log(restaurant.name + " " + restaurant.fullPageUri);
+			
+			var xhr = new XMLHttpRequest();
+			xhr.onload = function() {
+				if (xhr.readyState == 4)
+				{
+					var pageDoc = xhr.responseXML;
+					var addressElement = pageDoc.querySelector('span.address');
+
+					var restaurantAddress = "";
+
+					var streetAddressElement = addressElement.querySelector('span[itemprop="streetAddress"]');
+					if (streetAddressElement) restaurantAddress += streetAddressElement.innerHTML.trim();
+
+					var addressLocalityElement = addressElement.querySelector('span[itemprop="addressLocality"]')
+					if (addressLocalityElement) restaurantAddress += ", " + addressLocalityElement.innerHTML.trim(); 
+
+					var postcodeElement = addressElement.querySelector('span[itemprop="postalCode"]');
+					if (postcodeElement) restaurantAddress += ", " + postcodeElement.innerHTML.trim();
+					
+			  		var url = "http://api.ratings.food.gov.uk/Establishments?address=" + encodeURIComponent(restaurantAddress); 
+
+					var rating = 0;
+
+					var xhrFood = new XMLHttpRequest();
+					xhrFood.onreadystatechange = function() {
+						if (xhrFood.readyState == 4)
+						{
+							var resp = JSON.parse(xhrFood.responseText);
+							if (resp.establishments.length > 0) {
+								rating = resp.establishments[0].RatingValue;
+							}
+							else {
+								rating = -1;
+								//console.log(restaurant.id + " " + restaurant.name + " " + restaurantAddress);
+							}
+							port.postMessage({id:restaurant.id, rating:rating});
+						}
+					};
+					xhrFood.open("GET", url, true);
+					xhrFood.setRequestHeader('x-api-version', 2);
+					xhrFood.setRequestHeader('Content-Type','application/json');
+					xhrFood.setRequestHeader('Accept','application/json');
+					xhrFood.send();
+				}
+			};
+			xhr.open("GET", restaurant.fullPageUri);
+			xhr.responseType = "document";
+			xhr.send();
+	  	});
+  	}
 });
